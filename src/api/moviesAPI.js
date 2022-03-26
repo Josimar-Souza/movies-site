2c63ae2797bc93485aa77698ab31e981d075bf9b
Josimar Souza <contact.josimarsouza@gmail.com>
2022-03-26 18:46:18 -0300

    Created a class MoviesAPI to deal with api requests

---
+++ src/api/moviesAPI.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const baseQueryString = `?api_key=${apiKey}&language=pt-BR`;
+
+class MoviesAPI {
+  constructor(baseURL) {
+    this.http = axios.create({
+      baseURL,
+      timeout: 10000,
+    });
+  }
+
+  async getNowPlaying() {
+    const { data: { results } } = await this.http.get(`/movie/now_playing${baseQueryString}`);
+    return results;
+  }
+
+  async getPopular() {
+    const { data: { results } } = await this.http.get(`/movie/popular${baseQueryString}`);
+    return results;
+  }
+
+  async getTopRated() {
+    const { data: { results } } = await this.http.get(`/movie/top_rated${baseQueryString}`);
+    return results;
+  }
+
+  async getUpcoming() {
+    const { data: { results } } = await this.http.get(`/movie/upcoming${baseQueryString}`);
+    return results;
+  }
+}
+
+export default MoviesAPI;
