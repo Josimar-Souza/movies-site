d60bebc6e9bc995a42b64bdb1c43023dfebe9b56
Josimar Souza <contact.josimarsouza@gmail.com>
2022-03-28 09:11:49 -0300

    Created a base configuration for state using redux

---
+++ src/redux/actions/movies/actionsTypes.js
@@ -0,0 +1,7 @@
+const MOVIES_NOW_PLAYING = 'MOVIES_NOW_PLAYING';
+
+const actionsTypes = {
+  MOVIES_NOW_PLAYING,
+};
+
+export default actionsTypes;
---
+++ src/redux/reducers/movies/index.js
@@ -0,0 +1,23 @@
+import actionsTypes from '../../actions/movies/actionsTypes';
+
+const { MOVIES_NOW_PLAYING } = actionsTypes;
+
+const INITIAL_STATE = {
+  movies: {
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upcoming: [],
+  },
+};
+
+const movieReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case MOVIES_NOW_PLAYING:
+      return { ...state, nowPlaying: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default movieReducer;
