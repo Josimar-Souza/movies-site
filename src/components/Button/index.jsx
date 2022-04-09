be76accac1ee1ea0fbf5e8d6c1a8e0b27c719402
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-09 16:38:34 -0300

    Created method onButtonClick on Header

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -7,12 +7,14 @@ function Button(props) {
     children,
     onClick,
     width,
+    name,
   } = props;
 
   return (
     <ButtonStyle
       width={width}
       onClick={onClick}
+      name={name}
     >
       { children }
     </ButtonStyle>
@@ -22,12 +24,14 @@ function Button(props) {
 Button.defaultProps = {
   onClick: () => {},
   width: '50%',
+  name: '',
 };
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
   width: PropTypes.string,
+  name: PropTypes.string,
 };
 
 export default Button;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './HeaderStyles';
 import Button from '../Button';
 
 function Header() {
+  const navigate = useNavigate();
   const { HeaderContainer, HeaderStyle } = styles;
+
+  const onButtonClick = ({ target: { name } }) => {
+    if (name === 'series') {
+      navigate('/series');
+    } else {
+      navigate('/movies');
+    }
+  };
+
   return (
     <HeaderContainer>
       <HeaderStyle>
         <Button
           width="15%"
+          onClick={onButtonClick}
+          name="movies"
         >
           Filmes
         </Button>
         <Button
           width="15%"
+          onClick={onButtonClick}
+          name="series"
         >
           Séries
         </Button>
