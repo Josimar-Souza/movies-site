e2f9f9539588f347e10878b910951088cb62bb24
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-09 15:00:47 -0300

    Created action getAiringToday for series

---
+++ src/redux/actions/series/actions.js
@@ -0,0 +1,26 @@
+import actionsTypes from './actionsTypes';
+import SeriesAPI from '../../../api/seriesAPI';
+
+const {
+  SERIES_AIRING_TODAY,
+  SERIES_ON_THE_AIR,
+  SERIES_POPULAR,
+  SERIES_TOP_RATED,
+} = actionsTypes;
+
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+
+const seriesAPI = new SeriesAPI(baseURL);
+
+const getAiringToday = () => (dispatch) => {
+  seriesAPI.getAiringToday().then((payload) => dispatch({
+    type: SERIES_AIRING_TODAY,
+    payload,
+  }));
+};
+
+const actions = {
+  getAiringToday,
+};
+
+export default actions;
