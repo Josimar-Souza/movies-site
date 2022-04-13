23dde8ac1655d2cf21850592a62bf990d02876e4
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-13 14:33:58 -0300

    Added background to series details page

--- src/pages/SeriesDetails/SeriesDetailsStyles.js
+++ src/pages/SeriesDetails/SeriesDetailsStyles.js
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
+import getImageURL from '../../utils/getImageURL';
 
-const SeriesDetailsBackground = styled.div``;
+const getBackground = (props) => {
+  if (props.image == null) {
+    return 'background: #0f0f0f;';
+  }
+
+  return `background-image: url(${getImageURL(props.image)});`;
+};
+
+const SeriesDetailsBackground = styled.div`
+  ${getBackground}
+  background-size: cover;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: -1;
+`;
 
 const styles = {
   SeriesDetailsBackground,
