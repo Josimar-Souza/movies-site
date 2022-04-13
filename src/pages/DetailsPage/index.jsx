a24dee37c97ce905a753fdc40ab63ac1b850973f
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-13 19:09:38 -0300

    Added trailer

--- src/pages/DetailsPage/index.jsx
+++ src/pages/DetailsPage/index.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 import styles from './DetailsPage';
 import MoviesAPI from '../../api/moviesAPI';
 import getImageURL from '../../utils/getImageURL';
+import getTrailer from '../../utils/getTrailer';
+import Trailer from '../../components/Trailer';
 
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 const moviesAPI = new MoviesAPI(baseURL);
@@ -11,6 +13,7 @@ const moviesAPI = new MoviesAPI(baseURL);
 function DetailsPage({ type }) {
   const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState({});
+  const [movieVideos, setMovieVideos] = useState([]);
 
   const {
     DetailsBackgorund,
@@ -22,13 +25,30 @@ function DetailsPage({ type }) {
   useEffect(() => {
     const getMovieDetails = async () => {
       const details = await moviesAPI.getDetails(id);
+      const videos = await moviesAPI.getVideos(id);
       setMovieDetails(details);
+      setMovieVideos(videos);
     };
 
     if (type === 'movie') {
       getMovieDetails();
     }
   }, []);
+
+  const getYoutubeTrailer = () => {
+    let trailer = {};
+    if (type === 'movie') {
+      if (movieVideos.length > 0) {
+        trailer = getTrailer(movieVideos);
+      }
+    }
+
+    if (trailer) {
+      return trailer.key;
+    }
+
+    return '';
+  };
 
   console.log(movieDetails);
 
@@ -39,6 +59,8 @@ function DetailsPage({ type }) {
         <DetailsContainer>
           <DetailsImageTrailerContainer>
             <DetailsImage src={getImageURL(movieDetails.poster_path)} />
+            <h3>Trailer</h3>
+            <Trailer trailerKey={getYoutubeTrailer()} />
           </DetailsImageTrailerContainer>
         </DetailsContainer>
       </section>
