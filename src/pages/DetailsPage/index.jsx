637fdd9e04a6c86a73dca2f2aa445a5cf307e1de
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-13 18:57:23 -0300

    Added background image

--- src/pages/DetailsPage/index.jsx
+++ src/pages/DetailsPage/index.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './DetailsPage';
+import MoviesAPI from '../../api/moviesAPI';
+
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+const moviesAPI = new MoviesAPI(baseURL);
 
 function DetailsPage() {
+  const { id } = useParams();
+  const [movieDetails, setMovieDetails] = useState({});
+
+  const {
+    DetailsBackgorund,
+  } = styles;
+
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      const details = await moviesAPI.getDetails(id);
+      setMovieDetails(details);
+    };
+
+    getMovieDetails();
+  }, []);
+
+  console.log(movieDetails);
+
   return (
-    <h1>Details Page!</h1>
+    <DetailsBackgorund image={movieDetails.backdrop_path} />
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import MoviesPage from './MoviesPage';
 import SeriesPage from './SeriesPage';
 import MoviesDetails from './MoviesDetails';
 import SeriesDetails from './SeriesDetails';
+import DetailsPage from './DetailsPage';
 
 const pages = {
   MoviesPage,
   SeriesPage,
   MoviesDetails,
   SeriesDetails,
+  DetailsPage,
 };
 
 export default pages;
