67fb1abe904db3953cfa2a96d3ca6dc36a678813
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-13 14:49:28 -0300

    Added serie poster

--- src/pages/SeriesDetails/SeriesDetailsStyles.js
+++ src/pages/SeriesDetails/SeriesDetailsStyles.js
@@ -9,6 +9,27 @@ const getBackground = (props) => {
   return `background-image: url(${getImageURL(props.image)});`;
 };
 
+const SeriesDetailsImage = styled.img`
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+
+const SeriesDetailsImageTrailerContainer = styled.div`
+  text-align: center;
+  width: 40%;
+`;
+
+const SeriesDetailsContainer = styled.div`
+  border-radius: 15px;
+  background: rgba(48, 48, 48, 0.7);
+  color: white;
+  display: flex;
+  justify-content: space-around;
+  margin: 5rem auto;
+  padding: 2rem;
+  width: 85%;
+`;
+
 const SeriesDetailsBackground = styled.div`
   ${getBackground}
   background-size: cover;
@@ -21,6 +42,9 @@ const SeriesDetailsBackground = styled.div`
 
 const styles = {
   SeriesDetailsBackground,
+  SeriesDetailsContainer,
+  SeriesDetailsImageTrailerContainer,
+  SeriesDetailsImage,
 };
 
 export default styles;
--- src/pages/SeriesDetails/index.jsx
+++ src/pages/SeriesDetails/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './SeriesDetailsStyles';
 import SeriesAPI from '../../api/seriesAPI';
+import getImageURL from '../../utils/getImageURL';
 
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 const seriesAPI = new SeriesAPI(baseURL);
@@ -9,7 +10,13 @@ const seriesAPI = new SeriesAPI(baseURL);
 function SeriesDetails() {
   const { id } = useParams();
   const [serieDetails, setSerieDetails] = useState({});
-  const { SeriesDetailsBackground } = styles;
+
+  const {
+    SeriesDetailsBackground,
+    SeriesDetailsContainer,
+    SeriesDetailsImageTrailerContainer,
+    SeriesDetailsImage,
+  } = styles;
 
   useEffect(() => {
     const getSerieDetails = async () => {
@@ -23,7 +30,13 @@ function SeriesDetails() {
   console.log(serieDetails);
 
   return (
-    <SeriesDetailsBackground>Series Details!</SeriesDetailsBackground>
+    <SeriesDetailsBackground image={serieDetails.backdrop_path}>
+      <SeriesDetailsContainer>
+        <SeriesDetailsImageTrailerContainer>
+          <SeriesDetailsImage src={getImageURL(serieDetails.poster_path)} />
+        </SeriesDetailsImageTrailerContainer>
+      </SeriesDetailsContainer>
+    </SeriesDetailsBackground>
   );
 }
 
