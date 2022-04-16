8dd3401c16650972f698bcece4bfef3b5065fd2e
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-16 10:24:22 -0300

    Created loading component

---
+++ src/components/Loading/LoadingStyles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const LoadingStyle = styled.div``;
+
+export default LoadingStyle;
---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoadingStyle from './LoadingStyles';
+
+function Loading() {
+  return (
+    <LoadingStyle />
+  );
+}
+
+export default Loading;
