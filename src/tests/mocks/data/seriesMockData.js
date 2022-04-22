bad176685094af75e5f95d1c6ce0cb6f7ab789fa
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-22 15:57:15 -0300

    Created mock data for series api

---
+++ src/tests/mocks/data/seriesMockData.js
@@ -0,0 +1,42 @@
+const seriesMockData = {
+  airingToday: [
+    {
+      backdrop_path: '/AlknjdkfbiFhfgYtr765.jpg',
+      poster_path: '/Kjfmn765Gfjckjnf65D.jpg',
+      first_air_date: '2005-12-05',
+      id: '236197',
+      name: 'Série 1',
+      overview: 'Overview de Série 1',
+      vote_average: 6.7,
+    },
+    {
+      backdrop_path: '/AlknjdkfLkmgjhdy65765.jpg',
+      poster_path: '/Kjfmn765GLkmjhy76F.jpg',
+      first_air_date: '2010-01-01',
+      id: '452397',
+      name: 'Série 2',
+      overview: 'Overview de Série 2',
+      vote_average: 8.5,
+    },
+    {
+      backdrop_path: '/asdLkTkfLkmgjhdy65765.jpg',
+      poster_path: '/Lmj87hGbv256kmjhy76F.jpg',
+      first_air_date: '2014-03-17',
+      id: '329716',
+      name: 'Série 3',
+      overview: 'Overview de Série 3',
+      vote_average: 7.2,
+    },
+    {
+      backdrop_path: '/asdLKjmfhsu877dy65765.jpg',
+      poster_path: '/Lmj87hSklmzxcvaWy76F.jpg',
+      first_air_date: '2016-09-11',
+      id: '154329',
+      name: 'Série 4',
+      overview: 'Overview de Série 4',
+      vote_average: 9.2,
+    },
+  ],
+};
+
+export default seriesMockData;
