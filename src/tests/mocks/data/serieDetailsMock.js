99238b4afae2e189b2930554bfeed081f2ba499a
Josimar Souza <contact.josimarsouza@gmail.com>
2022-04-24 15:06:16 -0300

    Added mock data for series details

---
+++ src/tests/mocks/data/serieDetailsMock.js
@@ -0,0 +1,40 @@
+const serieDetailsMock = {
+  bacdrop_path: '/Lkjfop35ajfGer9.jpg',
+  first_air_date: '2016-03-30',
+  genres: [
+    {
+      id: 362498,
+      name: 'Aventura',
+    },
+    {
+      id: 563219,
+      name: 'Sci-Fi',
+    },
+    {
+      id: 876319,
+      name: 'Fantasia',
+    },
+  ],
+  id: 316587,
+  name: 'Série 1',
+  number_of_episodes: 66,
+  number_of_seasons: 3,
+  overview: 'Overview da série 1',
+  poster_path: '/Lkiu87hfGdvC.jpg',
+  production_companies: [
+    {
+      id: 321649,
+      logo_path: 'Lkfmbz987GfE.jpg',
+      name: 'Empresa 1',
+    },
+    {
+      id: 234618,
+      logo_path: 'MjhguBcvDeQ1.jpg',
+      name: 'Empresa 2',
+    },
+  ],
+  status: 'Returning Series',
+  vote_average: 7.9,
+};
+
+export default serieDetailsMock;
